f8181b8d9de9af2fd5f5bc16e9af69c5eda3c0fc
Maksim Shkrebtan <mshkrebtan@gmail.com>
2019-07-11 09:08:43 +0200

    Add notes in Russian

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,58 +4,90 @@ module.exports = {
 
   dest: 'docs',
   themeConfig: {
-    sidebarDepth: '4',
-    sidebar: [
-      {
-        title: 'Kindle',
-        children: [
-          '/kindle/best-way-to-convert-fb2-to-mobi',
-          '/kindle/convert-epub-to-mobi-with-kindlegen'
-        ]
-      },
-      {
-        title: 'Privacy',
-        children: [
-          '/privacy/spoof-your-browser-platform'
-        ]
-      },
-      {
-        title: 'Proxy & VPN',
-        children: [
-          '/proxy-and-vpn/configure-socks-proxy-on-ios-with-pac',
-          '/proxy-and-vpn/simple-dante-configuration'
-        ]
-      },
-      {
-        title: 'Linux',
-        children: [
-          '/linux/change-intel-i915-s-pwm-frequency-on-boot'
-        ]
-      },
-      {
-        title: 'macOS',
-        path: '/macos/',
-        children: [
-          '/macos/mount-ntfs-volume-for-writing',
-          '/macos/temporarily-prevent-mac-from-sleeping',
-          '/macos/silence-the-startup-chime',
-          '/macos/batch-convert-doc-to-pdf-with-libreoffice'
-        ]
-      }
-    ],
-
-    lastUpdated: 'Last Updated',
-  },
-
-      markdown: {
-        lineNumbers: true
-      },
-      plugins: [
-        [
-          '@vuepress/google-analytics',
+    locales: {
+      '/': {
+        selectText: 'Languages',
+        label: 'English',
+        sidebarDepth: '4',
+        sidebar: [
           {
-            'ga': 'UA-142563675-1'
+            title: 'Kindle',
+            children: [
+              '/kindle/best-way-to-convert-fb2-to-mobi',
+              '/kindle/convert-epub-to-mobi-with-kindlegen'
+            ]
+          },
+          {
+            title: 'Privacy',
+            children: [
+              '/privacy/spoof-your-browser-platform'
+            ]
+          },
+          {
+            title: 'Proxy & VPN',
+            children: [
+              '/proxy-and-vpn/configure-socks-proxy-on-ios-with-pac',
+              '/proxy-and-vpn/simple-dante-configuration'
+            ]
+          },
+          {
+            title: 'Linux',
+            children: [
+              '/linux/change-intel-i915-s-pwm-frequency-on-boot'
+            ]
+          },
+          {
+            title: 'macOS',
+            path: '/macos/',
+            children: [
+              '/macos/mount-ntfs-volume-for-writing',
+              '/macos/temporarily-prevent-mac-from-sleeping',
+              '/macos/silence-the-startup-chime',
+              '/macos/batch-convert-doc-to-pdf-with-libreoffice'
+            ]
           }
-        ]
-      ]
+        ],
+
+        lastUpdated: 'Last Updated'
+      },
+      '/ru/': {
+        selectText: 'Языки',
+        label: 'Русский',
+        sidebarDepth: '4',
+        sidebar: [
+          {
+            title: 'macOS',
+            path: '/ru/macos/',
+            children: [
+              '/ru/macos/nalog-ru-digital-signature'
+            ]
+          }
+        ],
+        lastUpdated: 'Последнее обновление'
+      }
+    }
+  },
+  markdown: {
+    lineNumbers: true
+  },
+  plugins: [
+    [
+      '@vuepress/google-analytics',
+      {
+        'ga': 'UA-142563675-1'
+      }
+    ]
+  ],
+  locales: {
+    '/': {
+      lang: 'en-GB',
+      title: 'Maxie’s Notes',
+      description: 'Maksim Shkrebtan’s personal documentation website'
+    },
+    '/ru/': {
+      lang: 'ru-RU',
+      title: 'Maxie’s Notes',
+      description: 'Личные заметки Максима Шкребтана'
+    }
   }
+}
